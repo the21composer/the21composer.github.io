5502faeed1c621c5c0c9a2dfb7b217292782df2a
the21composer <42894877+the21composer@users.noreply.github.com>
2020-01-27 23:49:43 +0300

    animated links

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 $(document).ready(function(){
 	// плавное перемещение страницы к нужному блоку
     jQuery("a.link").click(function () {
-        elementClick = jQuery(this).attr("id")
+        elementClick = jQuery(this).attr("id");
         destination = jQuery(elementClick).offset().top;
         jQuery("html:not(:animated),body:not(:animated)").animate({scrollTop: destination}, 400);
     });
+
+    jQuery(".download-btn,.soc-btn").click(function () {
+        elementClick = jQuery(this).attr("id");
+        window.open(elementClick, '_blank');
+    })
 });
