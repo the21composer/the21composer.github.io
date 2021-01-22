6f1fb31f380e3fb539fa6cb6654dc0cea8c98952
the21composer <42894877+the21composer@users.noreply.github.com>
2021-01-22 21:57:30 +0300

    Update dev page

--- code/src/Components/Dev/DevOutput.js
+++ code/src/Components/Dev/DevOutput.js
@@ -1,11 +1,42 @@
 import React from "react"
-import {Grid} from "@material-ui/core"
+import {BottomNavigation, Box, Button, ButtonGroup, Grid} from "@material-ui/core"
 import {PageTypo} from ".."
 import {MainCard} from "./MainCard"
 import {ReactBlock} from "./ReactBlock"
 import {RepairAppBlock} from "./RepairAppBlock"
 import {QtAppBlock} from "./QtAppBlock"
 import {AngularAppBlock} from "./AngularAppBlock";
+import withStyles from "@material-ui/core/styles/withStyles";
+
+const Footer = withStyles({
+    root:{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "space-between",
+        paddingLeft: 15,
+        paddingRight: 15,
+        fontFamily: "Montserrat"
+    }
+})(Grid)
+const IntroButton = withStyles({
+    root:{
+        height:40,
+        maxWidth:75,
+        minWidth:50,
+        fontFamily:'Montserrat',
+        fontWeight:'400',
+        borderRadius:20,
+        '&:hover':{
+            background:'rgba(255,221,176,0.15)',
+        },
+        '&:active':{
+            boxShadow:'0px 3px 10px #00000050'
+        },
+    },
+    label:{
+        textTransform:'none',
+    },
+})(Button);
 
 export const DevOutput = () => {
     return (
@@ -25,6 +56,24 @@ export const DevOutput = () => {
             </Grid>
             <Grid item xs={12} lg={6}>
                 <QtAppBlock/>
+            </Grid>
+            <Grid item xs={12}>
+                <BottomNavigation>
+                    <Footer xs={12}>
+                        <div>
+                            Александр Облизанов, 2021
+                        </div>
+                        <Box display="flex" justifyContent="flex-end" className='linkBox' padding="6px">
+                            <ButtonGroup variant="text">
+                                <IntroButton href='https://vk.com/forever_alove'> VK </IntroButton>
+                                <IntroButton href='https://www.instagram.com/the21composer'> Inst </IntroButton>
+                                <IntroButton
+                                    href='https://www.youtube.com/channel/UC331TmQoaB0-EocWLoAtzwQ'> YouTube </IntroButton>
+                                <IntroButton href='https://github.com/the21composer'> GitHub </IntroButton>
+                            </ButtonGroup>
+                        </Box>
+                    </Footer>
+                </BottomNavigation>
             </Grid>
         </Grid>
     )
